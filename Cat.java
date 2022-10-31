14b239601b3d44be08cc198e8380235124e0b4e9
Liudmyla Zhlobinska <l.zhlobinska@gmail.com>
2022-10-31 22:01:59 +0100

    New project

---
+++ Cat.java
@@ -0,0 +1,24 @@
+public class Cat {
+    private String name;
+    private String color;
+    private String age;
+
+    public Cat(String name, String color, int age) {
+        this.name = name;
+        this.color = color;
+        this.age = String.valueOf(age);
+    }
+
+    public void voice () {
+        System.out.println("meow");
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                ", age='" + age + '\'' +
+                '}';
+    }
+}
