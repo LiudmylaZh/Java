e340174e3acae849cf8bf186b0c61bb4a57d1f7d
Liudmyla Zhlobinska <l.zhlobinska@gmail.com>
2022-11-03 19:34:41 +0100

    debug

--- HelloCat.java
+++ HelloCat.java
@@ -1,9 +1,7 @@
 public class HelloCat {
     public static void main(String[] args) {
-        Cat cat = new Cat ("Barsik", "red", 4);
-                cat.voice();
-                System.out.println(cat);
-
-
+        Cat cat = new Cat("Barsik", "red", 4);
+        cat.voice();
+        System.out.println(cat);
     }
 }
