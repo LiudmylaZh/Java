59fdcb0b9c98ed8ec457f35b08becb62eb6de1c1
Liudmyla Zhlobinska <l.zhlobinska@gmail.com>
2022-11-10 21:26:38 +0100

    done homework 4

---
+++ homework4/homework4.java
@@ -0,0 +1,32 @@
+package homework4;
+/**
+ * Java Prof. Homework #3
+ *
+ * @author Liudmyla Zhlobinska
+ * @version 10.11 - 11.11
+ */
+
+import java.util.*;
+
+public class homework4 {
+    public static void main(String[] args) {
+        List<String> list = new LinkedList<>();
+        list.add("AAA");
+        list.add("BB");
+        list.add("CC");
+        list.add("DD");
+        list.remove("BB");
+        list.add(2, "BB");
+        list.add(2, "BB");
+
+        Set<String> set = new HashSet<>(list);
+        System.out.println(set);
+        Map<String, String> map = new HashMap<>();
+        map.put("Prague", "CZ");
+        map.put("Berlin", "DE");
+        System.out.println(map);
+        System.out.println(map.get("Brno"));
+
+
+    }
+}
